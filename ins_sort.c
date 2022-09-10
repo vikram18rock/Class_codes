91927475673531cdcf3528dc499c8228ad230587
vikram18rock <v16ash@gmail.com>
2022-09-10 12:23:59 +0530

    written function

--- ins_sort.c
+++ ins_sort.c
@@ -22,3 +22,17 @@ int main()
     printf("\n");
     return 0;
 }
+
+void insert_sort(int a[], int n)
+{
+    int key, i, j;
+    for (i = 1; i < n; i++)
+    {
+        key = a[i];
+        for (j = i-1; j >= 0 && a[j] > key; j--)
+        {
+            a[j+1] = a[j];
+        }
+        a[j+1] = key;
+    }
+}
