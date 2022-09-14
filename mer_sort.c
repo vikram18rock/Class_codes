62bc4e4bf25d657138e85da842b4cb4874f0ba76
vikram18rock <v16ash@gmail.com>
2022-09-14 13:45:32 +0530

    added comments for understanding

--- mer_sort.c
+++ mer_sort.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+// function declarations
 void merge_sort(int a[], int s, int e);
 void merge(int a[], int s, int m, int e);
 
 int main()
 {
+    // input
     int n;
     scanf("%d", &n);
 
@@ -14,8 +16,10 @@ int main()
         scanf("%d", &a[i]);
     }
     
+    // sorting the array
     merge_sort(a, 0, n-1);
 
+    // printing sorted array
     for (int i = 0; i < n; i++)
     {
         printf("%d ", a[i]);
@@ -26,20 +30,27 @@ int main()
 
 void merge_sort(int a[], int s, int e)
 {
+    // for middle element
     int m;
+
+    // checking whether algorithm reached single element or not
     if (s < e)
     {
         m = (s + e)/2;
+        // partitioning and recursion
         merge_sort(a, s, m);
         merge_sort(a, m+1, e);
+        // merging partitions
         merge(a, s, m, e);
     }
 }
 void merge(int a[], int s, int m, int e)
 {
+    // lengths of partitions
     int n1 = m-s+1;
     int n2 = e-m;
 
+    // declaring one more element width arrays
     int l[n1+1], r[n2+1];
 
     for (int i = 0; i < n1; i++)
@@ -50,9 +61,13 @@ void merge(int a[], int s, int m, int e)
     {
         r[i] = a[m + i + 1];
     }
+
+    // initialising last element
     l[n1] = 10000;
     r[n2] = 10000;
     int i = 0, j = 0;
+
+    // merging
     for(int k = s; k <= e; k++)
     {
         if (l[i] < r[j])
