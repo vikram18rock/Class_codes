17553927e972ae4478abb586534dda59a839eb99
Aadarsh <100817414+vikram18rock@users.noreply.github.com>
2022-12-02 18:44:17 +0530

    Update lin_search_iter.c
    copied the b_search iterative into it accidentally
    corrected the code now

--- Search_Algos/Linear_search/lin_search_iter.c
+++ Search_Algos/Linear_search/lin_search_iter.c
@@ -2,7 +2,7 @@
 
 //function declarations
 //returns required element index
-int binary_search(int *a, int n, int data);
+int lin_search(int *a, int n, int data);
 
 int main()
 {
@@ -19,7 +19,7 @@ int main()
     }
 
     //search function
-    res = binary_search(input, n, t);
+    res = lin_search(input, n, t);
 
     //print output
     printf("%d\n", res);
@@ -28,35 +28,14 @@ int main()
 }
 
 //definitions
-int binary_search(int *a, int n, int data)
+int lin_search(int *a, int n, int data)
 {
-    int mid, end1 = 0, end2 = n-1;
-
-    //to mid element
-    mid = (end1 + end2) / 2;
-
-    //for not repeating back if element not found
-    while(end1 <= end2)
-    {
-        //goes left
-        if(data < a[mid])
-        {
-            //new end1 end2 and mid to go left
-            end2 = mid - 1; 
-            mid = (end1 + end2) / 2;
-        }
-        else if(data == a[mid])
-        {
-            //return the resultof search
-            return mid;
-        }
-        else
-        {
-            //new end1 end2 and mid to go right side
-            end1 = mid + 1;
-            mid = (end1 + end2) / 2;
-        }
-    }
-    //returning -1 if element is not found
+    // checking through the loop
+    for(int i = 0; i < n; i++)
+        if(a[i] == data)
+            // return index if element is found 
+            return i;
+    
+    // element not found
     return -1;
 }
