ee2ff4aff7e2d3d888f5082b3684ab8257f7d483
Aadarsh <100817414+vikram18rock@users.noreply.github.com>
2022-12-02 17:31:04 +0530

    recursive approach

--- Search_Algos/Linear_search/lin_search.c
+++ Search_Algos/Linear_search/lin_search.c
@@ -1,3 +1,39 @@
 #include <stdio.h>
 
 int search_list(int a[], int len, int key)
+{
+    // decrease the index
+    len--;
+
+    // if the list is over
+    if(len < 0)
+        return len;
+    // list is not done and we found key
+    if(a[len] == key)
+    {
+        // return the found index
+        return len;
+    }
+    // key not found, then search remaining array
+    search_list(a, len, key);
+}
+
+int main()
+{
+    int n;
+    // size of input array
+    scanf("%d", &n);
+
+    int a[n];
+    // take input array
+    for(int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    int k;
+    scanf("%d", &k);
+
+    // print search result
+    printf("%d\n",search_list(a, n, k));
+
+    return 0;
+}
